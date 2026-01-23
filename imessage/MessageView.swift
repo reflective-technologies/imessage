eb97b4780c387c0e2e4a45ae29879931b9bf9c11
Hunter Diamond <huntertdiamond@gmail.com>
2026-01-22 20:43:09 -0500

    updates

---
+++ imessage/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  imessage
+//
+//  Created by hunter diamond on 1/22/26.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}
